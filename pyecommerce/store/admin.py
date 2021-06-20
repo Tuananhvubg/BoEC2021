41185b0f0ff341b81161009f9a1bb4cd67707980
tuan anh <tuananhvubg1999@gmail.com>
2021-06-20 21:19:57 +0700

    tuananhvu add project

---
+++ pyecommerce/store/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Customer, OrderReport, Product, Order, Tag, Comment, OrderItem, ShippingInformation, Payment
+# Register your models here.
+admin.site.register(Customer);
+admin.site.register(Product);
+admin.site.register(Order);
+admin.site.register(Tag);
+admin.site.register(Comment);
+admin.site.register(OrderItem);
+admin.site.register(ShippingInformation);
+admin.site.register(Payment);
+admin.site.register(OrderReport);
